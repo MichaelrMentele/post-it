f8e5fb76d73066d21831511f64ff3c36a3aa2c03
Michael Mentele <Michaelrmentele@gmail.com>
2016-09-28 09:11:48 -0700

    Added user auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,24 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # makes this availbe in all templates
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    # the ||= is a memorization. Says if it exists don't run the code. 
+    # The curent user is available on all controllers
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def require_user 
+    if !logged_in?
+      flash[:error] = "You aren't logged in."
+      redirect_to root_path
+    end
+  end
 end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,53 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+  before_action :require_same_user, only: [:edit, :update]
+
+  def new
+    @user = User.new
+  end
+
+  def show
+    
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:notice] = "You are registered"
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "Your profile was updated."
+      redirect_to user_path
+    else
+      render :edit
+    end
+  end
+
+  def edit
+
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+
+  def require_same_user
+    if current_user != @user
+      flass[:error] = "You can't do that"
+    end
+  end
+end
