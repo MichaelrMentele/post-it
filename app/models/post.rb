92ff464363a740ef99de3a42aca25e78c3b69085
Michael Mentele <Michaelrmentele@gmail.com>
2016-10-03 08:59:23 -0700

    Added slugging and voteable gem

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,50 +1,15 @@
 class Post < ActiveRecord::Base
   include Voteable
-  
+  include Sluggable
+
   belongs_to :creator, foreign_key: "user_id", class_name: "User"
   has_many :comments
   has_many :post_categories
   has_many :categories, through: :post_categories
-  has_many :votes, as: :voteable
 
   validates :title, presence: true, length: {minimum: 5}
   validates :url, presence: true, uniqueness: true
   validates :description, presence: true
 
-  before_save :generate_slug
-
-  def to_param
-    self.slug
-  end
-
-  def generate_slug
-    new_slug = to_slug(self.title)
-
-    # if a post with this slug exists, uniquify it
-    post = Post.find_by slug: new_slug
-
-    count = 2
-    while post && post != self
-      new_slug = append_suffix(new_slug, count)
-      post = Post.find_by slug: new_slug
-      count += 1
-    end
-
-    self.slug = new_slug
-  end
-
-  def append_suffix(str, count)
-    if str.split('-').last.to_i != 0
-      return str.split("-").slice(0...-1).join("-") + "-" + count.to_s
-    else
-      return str + "-" count.to_s
-    end
-  end
-
-  def to_slug(name)
-    str = name.strip.downcase
-    str.gsub!(/\s*[^a-z0-9]\s*/, "-")
-    str.gsub!(/-+/, "-")
-    return str
-  end
+  sluggable_column :title
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  include Sluggable
+
   has_many :posts
   has_many :comments
   has_many :votes
@@ -8,40 +10,13 @@ class User < ActiveRecord::Base
   validates :username, presence: true, uniqueness: true
   validates :password, presence: true, on: :create
 
-  before_save :generate_slug
+  sluggable_column :username
 
-  def to_param
-    self.slug
+  def admin?
+    self.role == 'admin'
   end
 
-  def generate_slug
-    new_slug = to_slug(self.username)
-
-    # if a user with this slug exists, uniquify it
-    user = User.find_by slug: new_slug
-
-    count = 2
-    while user && user != self
-      new_slug = append_suffix(new_slug, count)
-      user = User.find_by slug: new_slug
-      count += 1
-    end
-
-    self.slug = new_slug
-  end
-
-  def append_suffix(str, count)
-    if str.split('-').last.to_i != 0
-      return str.split("-").slice(0...-1).join("-") + "-" + count.to_s
-    else
-      return str + "-" count.to_s
-    end
-  end
-
-  def to_slug(name)
-    str = name.strip.downcase
-    str.gsub!(/\s*[^a-z0-9]\s*/, "-")
-    str.gsub!(/-+/, "-")
-    return str
+  def moderator?
+    self.role == 'moderator'
   end
 end
